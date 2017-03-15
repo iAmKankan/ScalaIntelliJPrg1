92414791aad9e65fa858458f98b2a9d3ea1a4326
arijitBhadra <bhadraarijit84@gmail.com>
2017-03-15 16:51:56 +0530

    March 9 2017

--- src/homeWork/EmployeeZica.scala
+++ src/homeWork/EmployeeZica.scala
@@ -9,7 +9,7 @@ class Employee(name:String,age:Int,sal:Double,add:String){
   override def toString: String = "Name "+name+" Age "+age+" Salary "+sal+" Address "+add
 }
 
-object EmployeeZica {
+object EmployeeZeeca {
   def main(args: Array[String]): Unit = {
     val e1=new Employee("Arijit",25,70000.0,"mallikpur")
     val e2=new Employee("Ankith",30,90000.0,"merath")
